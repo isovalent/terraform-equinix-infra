4dcd0d03f2afeedf9d8e98ed573054bf6880230a
Liyi Huang <pdshly@gmail.com>
2024-07-03 16:40:22 -0400

    commits

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "host-public-ip-address" {
   description = "the public IP address of the equinix host"
-  value = local.equinix_public_network_host_ipv4_no_mask
+  value       = local.equinix_public_network_host_ipv4_no_mask
 
 }
 
 output "router-public-ip-address" {
   description = "the public IP address of the router between public and private network"
-  value = local.equinix_public_network_router_ipv4_no_mask
+  value       = local.equinix_public_network_router_ipv4_no_mask
 
 }
 
 output "testbox-public-ip-address" {
   description = "the public IP address of the testbox"
-  value = local.equinix_public_network_test_box_ipv4_no_mask
+  value       = local.equinix_public_network_test_box_ipv4_no_mask
 
 }
 
 output "k8s_master_ip_mac_hostname_maps" {
   description = "the public IP address of the testbox"
-  value = local.k8s_master_ip_mac_hostname_map
+  value       = local.k8s_master_ip_mac_hostname_map
 }
 output "k8s_worker_ip_mac_hostname_maps" {
   value = local.k8s_worker_ip_mac_hostname_map
