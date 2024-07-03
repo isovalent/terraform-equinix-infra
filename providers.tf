03ff87e34afcc0f5eea8b076c284970a76b9d924
Liyi Huang <pdshly@gmail.com>
2024-07-03 17:25:36 -0400

    fix the provider key name

--- providers.tf
+++ providers.tf
@@ -3,5 +3,5 @@ provider "equinix" {
 }
 # we have a hack to enforce the provider will be created after the host in libvirt-host.tf
 provider "libvirt" {
-  uri = "qemu+ssh://${local_file.host-username.content}@${equinix_metal_device.libvirt_host.network.0.address}/system?sshauth=privkey&known_hosts_verify=ignore&keyfile=${path.module}/output/${var.infra_name}_id_rsa"
+  uri = "qemu+ssh://${local_file.host-username.content}@${equinix_metal_device.libvirt_host.network.0.address}/system?sshauth=privkey&known_hosts_verify=ignore&keyfile=${local_file.private_key_file.filename}"
 }
